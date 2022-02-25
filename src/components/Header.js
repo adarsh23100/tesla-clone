656cad0d6dfacd436d9f103d58ebf0aa14d9f331
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-02-25 16:32:01 +0530

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
+import {selectCars} from  '../features/car/carSlice'
+import {useSelector} from 'react-redux'
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+
   return (
     <Container>
         <a>
             <img src="/images/logo.svg" alt=""/>
         </a>
         <Menu>
-            <a href="#">Model S</a>
-            <a href="#">Model Y</a>
-            <a href="#">Model 3</a>
-            <a href="#">Model X</a>
+            {cars && cars.map((car,index) => (
+                <a key={index} href="#">{car}</a>
+            ))}
         </Menu>
         <RightMenu>
             <a href="#">Shop</a>
             <a href="#">Tesla Account</a>
-            <CustomMenu />
+            <CustomMenu onClick={() => setBurgerStatus(true)}/>
         </RightMenu>
-        <BurgerNav>
+        <BurgerNav show={burgerStatus}>
             <CloseWrapper>
-              <CustomClose />
+              <CustomClose onClick={() => setBurgerStatus(false)}/>
             </CloseWrapper>
+            {cars && cars.map((car,index) => {
+                <li key={index} ><a href="#">{car}</a></li>
+            })}
             <li><a href="#">Existing Inventory</a></li>
             <li><a href="#">Used Inventory</a></li>
             <li><a href="#">Trade-in</a></li>
             <li><a href="#">Cyber Truck</a></li>
             <li><a href="#">Roadster</a></li>
-            <li><a href="#">Existing Inventory</a></li>
-            <li><a href="#">Existing Inventory</a></li>
-            <li><a href="#">Existing Inventory</a></li>
         </BurgerNav>
     </Container>
   )
@@ -97,6 +101,11 @@ const BurgerNav = styled.div`
     display: flex;
     flex-direction: column;
     text-align: start;
+
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+
+    transition: transform 0.2s ease-in-out;
+
     li{
         padding: 15px 0;
         border-bottom: 1px solid rgba(0,0,0,0.2);
--- src/components/Section.js
+++ src/components/Section.js
@@ -43,6 +43,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
+    scroll-behavior: smooth;
     background-image: ${props => `url("/images/${props.bgImage}")`}
 `
 
